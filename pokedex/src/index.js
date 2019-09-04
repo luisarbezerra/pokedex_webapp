f1c413878e6ae9387df708e45d43059a1eda61cc
Luísa Ribeiro <luisarbezerra@hotmail.com>
2019-09-04 18:17:59 -0300

    [WIP] create-react-app + Redux setup

---
+++ pokedex/src/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
+import configureStore from './store';
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render(
+    <Provider store={configureStore()}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
+serviceWorker.unregister();
