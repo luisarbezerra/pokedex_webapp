19563b5a9ea1927eb94acd51227043ae469473b3
Luísa Ribeiro <luisarbezerra@hotmail.com>
2019-09-08 21:00:53 -0300

    Scroll infinito corrigido e funcionando de melhor forma

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -5,14 +5,18 @@ import Card from '../Card/Card'
 export default class Content extends React.Component {
     componentDidMount() {
         const pokemonLength = this.props.all_pokemon.length;
-        this.props.fetchPokemon(pokemonLength, 30);
-    
-        return (window.onscroll = () => {
-            debugger
-          if (!this.props.loading_pokemon && (window.innerHeight + window.scrollY) >= document.body.offsetHeight) {
-            this.props.fetchPokemon(this.props.all_pokemon.length, 30);
-          }
-        });
+        this.props.fetchPokemon(pokemonLength, 20);
+
+        window.addEventListener("scroll", function(){
+            var wrap = document.getElementById('content');
+            var contentHeight = wrap.offsetHeight;
+            var yOffset = window.pageYOffset; 
+            var y = yOffset + window.innerHeight;
+            if(!this.props.loading_pokemon && (y >= contentHeight))
+            {
+                this.props.fetchPokemon(this.props.all_pokemon.length, 20);
+            }
+        }.bind(this))
     }
 
     renderCard = (pokemon, key) => (
@@ -26,7 +30,7 @@ export default class Content extends React.Component {
     
     render() {
         return (
-            <div className="content">
+            <div className="content" id="content">
                 <div className="cards-table"> 
                     {this.renderCards()}
                 </div>
